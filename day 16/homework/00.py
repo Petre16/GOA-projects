e0c589afe10387838881d1d6689cad9524dbb948
“Petre16“ <sikmashvili.petre16@gmail.com>
2024-12-03 17:39:27 +0400

    commit

---
+++ day 16/homework/00.py
@@ -0,0 +1,5 @@
+print(True or False)
+print(False or True)
+
+print(True and False)
+print(False and True)
---
+++ day 16/homework/01.py
@@ -0,0 +1,12 @@
+bool1 = True
+bool2 = False
+
+print(bool1 or bool2)
+print(bool2 or bool1)
+print(bool1 or bool1)
+print(bool2 or bool2)
+
+print(bool1 and bool2)
+print(bool2 and bool1)
+print(bool1 and bool1)
+print(bool2 and bool2)
