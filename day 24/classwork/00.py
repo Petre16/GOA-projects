81577e3f1f778616520878396b051a46d65bd4ee
“Petre16“ <sikmashvili.petre16@gmail.com>
2025-01-02 22:48:50 +0400

    commit

---
+++ day 24/classwork/00.py
@@ -0,0 +1,7 @@
+user_number = int(input("Enter your number: "))
+
+if user_number > 10:
+    print("bigger than 10")
+
+else:
+    print("smaller than 10")
---
+++ day 24/classwork/01.py
@@ -0,0 +1,8 @@
+# მომხმარებელს შემოატანინეთ ინფორმაცია input-ით. თუ შემოიტანს "yes"-ს ან "Yes"-ს, მაშინ შექმენით is_student ცვლადი და გაუტოლეთ True-ს. სხვა შემთხვევაში (else) შექმენით is_student ცვლადი და გაუტოლეთ False-ს.
+
+is_student = str(input("Enter str: "))
+
+if is_student <= "Yes" or "yes":
+    print("is_student_True")
+else:
+    print("is_student_True")
